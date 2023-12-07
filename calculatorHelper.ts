3879d74c83c27972a2e19d7a945099cc7b04e1bd
samartawil <151541020+samartawil@users.noreply.github.com>
2023-12-07 13:18:49 +0200

    Update calculatorHelper.ts

--- calculatorHelper.ts
+++ calculatorHelper.ts
@@ -1,31 +1,24 @@
 export const handleEq = (expression: string): string => {
-  const order: string[] = ['²', '%', '√', '()'];
-
+  const order: string[] = ['²', '√', '()'];
   while (expression.match(/[+\-*/]/)) {
     for (const vertex of order) {
-      if (vertex === '²') {
-        expression = handleSq(expression);
-      } else if (vertex === '√') {
-        expression = handleSqr(expression);
-      } else if (vertex === '()') {
-        expression = handleParentheses(expression);
-      }
+      if (vertex === '²') expression = handleSq(expression);
+      else if (vertex === '√') expression = handleSqr(expression);
+      else if (vertex === '()') expression = handleParentheses(expression);
     }
     expression = handleOperations(expression, ['*', '/']);
     expression = handleOperations(expression, ['+', '-']);
   }
-
   return expression;
 };
+
 export const handleSq = (expression: string): string => {
 expression = String(expression);
 const squareRegex = /(-?\d+(\.\d+)?)\s*²/;
 while (expression.match(squareRegex)) {
   expression = expression.replace(squareRegex, (match, value) => {
     const numValue = parseFloat(value);
-    if (!isNaN(numValue)) {
-      return (numValue * numValue).toString();
-    }
+    if (!isNaN(numValue)) return (numValue * numValue).toString();
     return match;
   });
 }
@@ -39,9 +32,7 @@ export const handleSqr = (expression: string): string => {
     expression = expression.replace(sqrtRegex, (match, value) => {
       const numValue = parseFloat(value);
       if (!isNaN(numValue)) {
-        if (numValue < 0) {
-          return 'Error';
-        }
+        if (numValue < 0) return 'Error';
         return Math.sqrt(numValue).toString();
       }
       return match;
@@ -50,18 +41,12 @@ export const handleSqr = (expression: string): string => {
   return expression;
 };
 
-export const handlePerc = (input: string): string => {
-  return input + '/100';
-};
-
 export const handleParentheses = (expression: string): string => {
   expression = String(expression);
   while (expression.includes('(')) {
     const startIndex = expression.lastIndexOf('(');
     const endIndex = expression.indexOf(')', startIndex);
-    if (endIndex === -1) {
-      return 'Error';
-    }
+    if (endIndex === -1) return 'Error';
     const subExpression = expression.slice(startIndex + 1, endIndex);
     const subResult = handleEq(subExpression);
     expression = expression.slice(0, startIndex) + subResult + expression.slice(endIndex + 1);
@@ -77,32 +62,21 @@ export const handleOperations = (expression: string, operators: string[]): strin
       return result;
     });
   });
-
   return expression;
 };
 
 export const calculate = (left: string, right: string, operator: string): string => {
   const leftValue = parseFloat(left);
   const rightValue = parseFloat(right);
-
   if (isNaN(leftValue) || isNaN(rightValue)) {
     return 'Error';
   }
-
   switch (operator) {
-    case '+':
-      return (leftValue + rightValue).toString();
-    case '-':
-      return (leftValue - rightValue).toString();
-    case '*':
-      return (leftValue * rightValue).toString();
-    case '/':
-      if (rightValue === 0) {
-        return 'Error';
-      } else {
-        return (leftValue / rightValue).toString();
-      }
-    default:
-      return 'Error';
+    case '+': return String(leftValue + rightValue);
+    case '-': return String(leftValue - rightValue);
+    case '*': return String(leftValue * rightValue);
+    case '/': return (rightValue === 0) ? 'Error' : (leftValue === 0) ? 'Infinity' : String(leftValue / rightValue);
+    default: return 'Error';
   }
 };
+
